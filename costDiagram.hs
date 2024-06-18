ba4ab83ac338424d8e9c20a32e9e4ac7d39982d6
Juan Miguel Vilar <jvilar@uji.es>
2024-06-18 16:50:58 +0200

    Added margin to the costDiagram

--- costDiagram.hs
+++ costDiagram.hs
@@ -1,11 +1,11 @@
 import Model
 
-import Diagrams.Prelude(Diagram)
+import Diagrams.Prelude
 import Diagrams.Backend.Cairo(B)
 import Diagrams.Backend.Cairo.CmdLine
 
 costs :: Int -> Int -> Int -> Int -> Int -> Diagram B
-costs size a b k p = diagramTimes (instances size a b) k p
+costs size a b k p = diagramTimes (instances size a b) k p # centerXY # pad 1.1
 
 main = mainWith costs
 
